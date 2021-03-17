114715ae3d5abd4b0aaa85e8da5d7ec3ff39239c
Alexander Nestorov <jak.racer@gmail.com>
2021-03-17 21:36:36 +0200

    Uploaded solution

---
+++ 06 Client Side Rendering/Exercise/Task04/dropdown.js
@@ -0,0 +1,61 @@
+import { html, render } from '../node_modules/lit-html/lit-html.js'
+
+//update: (executed on app start and on every app operation)
+// interpolate template with data 
+// render result in the menu div
+
+//add function: (executed when the user clicks submit)
+//parse input value 
+// create object
+// sent POST request with object
+// on succes -> add result to local list and update template result
+
+const selectTemplate = (list) => html`
+<select id="menu">
+    ${list.map(x => html`
+    <option value=${x._id}>${x.text}</option>`)}
+</select>
+`;
+
+const endpoint = 'http://localhost:3030/jsonstore/advanced/dropdown';
+const main = document.querySelector('div');
+const input = document.getElementById('itemText');
+initialize();
+
+async function initialize() {
+
+    document.querySelector('form')
+        .addEventListener('submit', (event) => addItem(event,list));
+
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    const list = Object.values(data);
+
+    update(list);
+
+}
+
+function update(list) {
+    const result = selectTemplate(list);
+    render(result, main);
+}
+
+async function addItem(event,list) {
+    event.preventDefault();
+
+    const item = {
+        text: input.value
+    };
+
+    const response = await fetch(endpoint, {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(item)
+    });
+
+    const result = await response.json();
+    list.push(result);
+
+    update(list);
+    document.getElementById('itemText').value = '';
+}
